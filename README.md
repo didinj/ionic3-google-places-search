282f3cb8b1291cddda2af19295fbdbd1f96d24c6
didin jamaludin <didinj@gmail.com>
2018-03-03 06:33:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Places Search using Ionic 3, Angular 5 and Google Maps API
+
+This source code is part of [Places Search using Ionic 3, Angular 5 and Google Maps API]() tutorial.
+
+To run locally:
+
+* Clone this repo
+* Run `npm install`
+* Run `ionic serve -l`
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+declare var google;
+let map: any;
+let infowindow: any;
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
 
 @Component({
   selector: 'page-home',
@@ -7,8 +16,61 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
 
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    platform.ready().then(() => {
+      this.initMap();
+    });
   }
 
+  initMap() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: {lat: location.coords.latitude, lng: location.coords.longitude},
+        zoom: 15
+      });
+
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: {lat: location.coords.latitude, lng: location.coords.longitude},
+        radius: 1000,
+        type: ['store']
+      }, (results,status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+        }
+      });
+    }, (error) => {
+      console.log(error);
+    }, options);
+  }
+
+  createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeLoc,
+      icon: image
+    });
+    console.log(place);
+    google.maps.event.addListener(marker, 'click', function() {
+      // infowindow.setContent(place.name);
+      // infowindow.open(map, this);
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+        'Place ID: ' + place.place_id + '<br>' +
+        place.vicinity + '</div>');
+      infowindow.open(map, this);
+    });
+  }
 }
